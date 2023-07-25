4d86764cace651f2ecf3d61602b1c5b9c66cbac4
Lucas Hollas <33184880+LucasHollas@users.noreply.github.com>
2023-07-25 13:42:50 -0300

    Update Contents.swift

--- Variaveis/Variaveis.playground/Contents.swift
+++ Variaveis/Variaveis.playground/Contents.swift
@@ -4,3 +4,14 @@ var idade = 24
 let profissao = "desenvolvedor"
 
 print("O meu nome é \(nome), e tenho \(idade) anos e sou \(profissao)")
+
+var coeficienteRendimento: Double = 9.5
+var possuiHistoricoReprovacao: Bool
+possuiHistoricoReprovacao = false
+
+var turnoEmQueEstuda: Character = "D"
+
+typealias Estudante = String
+
+var aluno1 : Estudante = "João"
+var aluno1 : Estudante = "Ana"
