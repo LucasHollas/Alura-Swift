94f4af1317929602cca1fee2c1cbf9be3c55942b
lucashollas <devlucashollas@gmail.com>
2023-07-25 13:25:04 -0300

    Feat(Desafio): 'Desafio swift'

---
+++ Desafio/Desafio.playground/Contents.swift
@@ -0,0 +1,14 @@
+var InicialNome = "L"
+var Nome = "Lucas"
+var Apelido = "Hollas"
+var Idade = 24
+var Altura = "187cm"
+var Swift = true
+
+
+print("Inicial do nome: \(InicialNome)")
+print("Nome: \(Nome)")
+print("Apelido: \(Apelido)")
+print("Idade: \(Idade)")
+print("Altura: \(Altura)")
+print("Gosta de Swift: \(Swift)")
