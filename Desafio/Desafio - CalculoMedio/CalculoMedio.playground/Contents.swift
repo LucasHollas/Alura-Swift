cb7e1cd74ce49d17f8cec02eed067e3707337490
lucashollas <devlucashollas@gmail.com>
2023-07-25 22:59:37 -0300

    Feat(Desafios): 'Desafios restantes do módulo1 do curso de swift'

---
+++ Desafio/Desafio - CalculoMedio/CalculoMedio.playground/Contents.swift
@@ -0,0 +1,12 @@
+typealias formatNota = Double
+
+var notaLucas : formatNota = 8.5
+var notaJoao : formatNota = 5.5
+var notaMaria : formatNota = 10.0
+
+var notaMedia : formatNota = (notaLucas + notaJoao + notaMaria) / 3
+
+print("A media da turma e \(notaMedia)")
+
+
+    
---
+++ Desafio/Desafio - TelaQuente/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+var c : Double = 5.0
+var f : Double
+
+f = c * 9 / 5 + 32
+
+print("A temperatura de \(c)ºC sao \(f)ºF")
