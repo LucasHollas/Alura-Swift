7bab36de23e349a304214f28649e42a000611e9e
lucashollas <devlucashollas@gmail.com>
2023-07-25 23:09:00 -0300

    feat(Operadores): 'Exemplos de operadores artitmeticos'

---
+++ Operadores/OperadoresAritmeticos.playground/Contents.swift
@@ -0,0 +1,28 @@
+10 + 2
+10 - 2
+8 * 2
+10 / 2
+5.0 / 2 //Conversao
+5 % 2 //Modulo
+4 % 2
+
+5 + 2 * 10
+(5 + 2) * 10
+
+var numero1 = 10
+var numero2 = 50
+
+var soma = numero1 + numero2
+
+print(soma)
+
+numero1 += numero2
+
+numero1 -= 2
+
+var nome = "Lucas"
+var sobrenome = "Hollas"
+
+print(nome + "" + sobrenome)
+
+print("Meu nome e: \(nome) \(sobrenome)")
